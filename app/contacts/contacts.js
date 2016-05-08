5fc68b57b0e8a3048f7d6161342f873c68fbc687
paras <rockypro92@gmail.com>
2016-05-08 20:54:00 +0530

    Initialize repo, installed bower components and set controllers

---
+++ app/contacts/contacts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'ContactsCtrl'
+  });
+}])
+
+.controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
+  
+}]);
