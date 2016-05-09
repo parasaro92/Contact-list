0380d10345ca47c8e717d4194e498aed810acd63
paras <rockypro92@gmail.com>
2016-05-09 20:01:59 +0530

    Successfully added create and read functionality to contacts

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -9,6 +9,101 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
   });
 }])
 
+// Contacts controller
 .controller('ContactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-  
+  var ref = new Firebase('https://contact-list-app.firebaseio.com/contacts');
+
+  // get Contacts
+  $scope.contacts = $firebaseArray(ref);
+  // console.log($scope.contacts);
+
+  // Show Form
+  $scope.showAddForm = function() {
+    $scope.addFormShow = true;
+  }
+
+  //Hide Form
+  $scope.hide = function() {
+    $scope.addFormShow = false;
+  }
+
+  //Submit COntact
+  $scope.addFormSubmit = function() {
+    console.log("adding");
+
+    // Assign values
+    if($scope.name){ var name= $scope.name} else { var name = null; }
+    if($scope.email){ var email= $scope.email} else { var email = null; }
+    if($scope.company){ var company= $scope.company} else { var company = null; }
+    if($scope.work_phone){ var work_phone= $scope.work_phone} else { var work_phone = null; }
+    if($scope.mobile_phone){ var mobile_phone= $scope.mobile_phone} else { var mobile_phone = null; }
+    if($scope.home_phone){ var home_phone= $scope.home_phone} else { var home_phone = null; }
+    if($scope.street_address){ var street_address= $scope.street_address} else { var street_address = null; }
+    if($scope.city){ var city= $scope.city} else { var city = null; }
+    if($scope.state){ var state= $scope.state} else { var state = null; }
+    if($scope.zipcode){ var zipcode= $scope.zipcode} else { var zipcode = null; }
+
+    // Build Object
+    $scope.contacts.$add({
+      name: name,
+      email: email,
+      company: company,
+      phones:[{
+        mobile: mobile_phone,
+        home: home_phone,
+        work: work_phone
+      }],
+      address: [{
+        street_address: street_address,
+        city: city,
+        state: state,
+        zipcode:zipcode
+      }]
+    }).then(function(ref){
+      var id = ref.key();
+      console.log('Added Contact with ID: '+id);
+
+      // Clean Form
+      clearFields();
+
+      // Hide Form
+      $scope.addFormShow = false;
+
+      //Send Message
+      $scope.msg = "Contact Added";
+    });
+  }
+
+  $scope.showContact = function(contact) {
+    console.log("Show");
+
+    $scope.name = contact.name;
+    $scope.email = contact.email;
+    $scope.company = contact.company;
+    $scope.work_phone = contact.phones[0].work;
+    $scope.home_phone = contact.phones[0].home;
+    $scope.mobile_phone = contact.phones[0].mobile;
+    $scope.street_address = contact.address[0].street_address;
+    $scope.city = contact.address[0].city;
+    $scope.state = contact.address[0].state;
+    $scope.zipcode = contact.address[0].zipcode;
+
+    $scope.contactShow = true;
+  }
+
+  // Clear Fields
+  function clearFields() {
+    console.log("Clearing All Fields");
+
+    $scope.name = '';
+    $scope.email = '';
+    $scope.company = '';
+    $scope.mobile_phone = '';
+    $scope.home_phone = '';
+    $scope.work_phone = '';
+    $scope.street_address = '';
+    $scope.city = '';
+    $scope.state = '';
+    $scope.zipcode = '';
+  }
 }]);
